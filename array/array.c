201996e6adf93828f7d4ce7758b6d4c11d9c88c5
apynu <apynu@duck.com>
2025-03-09 08:32:13 +0100

    rename array function to carray

--- array/array.c
+++ array/array.c
@@ -2,43 +2,7 @@
 // NOTE: needs to be build with -fno-stack-protector flag, otherwise the
 // compiler will throw a stack smashing error
 
-// DAMN YOU OS MEMORY MANAGER!!!!!!! xD
-/*
-int main(void) {
-  // object is the object that is being stored size times
-  // size is the size of the array
-  // int* to the first object in the array
-  int object = 3;
-  int size = 10;
-
-  int *arrayp = &object;
-  printf("arrayp init value: %d\n", *arrayp);
-  printf("\n");
-
-  for (int i = 0; i < size; i++) {
-    arrayp++;
-    // fill up everything with 10s except for the first object which will be a 3
-    *arrayp = i + 5;
-    printf("Arrayp %d value: %d\n", i, *arrayp);
-    printf("Prev Arrayp %d value: %d\n", i, *(arrayp - 1));
-    arrayp--;
-    printf("\n");
-  }
-
-  if (arrayp != NULL) {
-    printf("hell\n");
-    printf("curr arrayp value: %d", *(arrayp - 9));
-
-    int *firstvalue = arrayp - 9;
-
-    printf("FirstArray value: %d", *firstvalue);
-  }
-
-  return 0;
-}
-*/
-
-int *array(int element1, int element2, int element3) {
+int *carray(int element1, int element2, int element3) {
   // assuming that the next 3 int sized memory adresses are unallocated
   int *arrayp = &element1;
 
@@ -52,6 +16,6 @@ int *array(int element1, int element2, int element3) {
 
 int main(void) {
   int element2 = 10;
-  printf("arrayp: %d", *(array(3, element2, 4) + 1));
+  printf("arrayp: %d", *(carray(3, element2, 4) + 1));
   return 0;
 }
