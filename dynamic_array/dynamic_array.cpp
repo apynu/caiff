b67c016b79c97bede4d74c3fc15c69876fe9790a
apynu <apynu@duck.com>
2025-03-12 16:59:04 +0100

    doesn't work yet

--- dynamic_array/dynamic_array.cpp
+++ dynamic_array/dynamic_array.cpp
@@ -3,18 +3,33 @@
 
 template <class T> class DynamicArray {
 public:
-  DynamicArray() {};
+  DynamicArray(void) : internalarraysize(0) { internalarray[0]; };
+  DynamicArray(int size) : internalarraysize(size) { internalarray[size]; };
 
   ~DynamicArray() {};
 
   void push_back(T element) {
+    currarrayoccupation += 1;
+    if (currarrayoccupation == internalarraysize) {
+      int *tmparray[internalarraysize + 1];
+      for (int i = 0; i < internalarraysize; i++) {
+        tmparray[i] = internalarray[i];
+      }
+      tmparray[internalarraysize] = &element;
 
+      internalarray = tmparray;
+    }
   };
 
 private:
+  int currarrayoccupation;
+  int internalarraysize;
+  int *internalarray[];
 };
 
 int main(void) {
+  DynamicArray<int> myvec;
+  myvec.push_back(10);
   std::cout << "Hello World" << std::endl;
   return 0;
 }
