b367fc740b21ad434d0853212efb6930c4b3fc23
apynu <apynu@duck.com>
2025-04-05 12:16:15 +0200

    add stack implementation

--- src/stack/stack.c
+++ src/stack/stack.c
@@ -1,6 +1,47 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+struct Element {
+  struct Element *previousElement;
+  int value;
+};
+
+struct Element *push(struct Element *LastElement, int value) {
+  struct Element *NewElement = calloc(1, sizeof(struct Element));
+  NewElement->previousElement = LastElement;
+  NewElement->value = value;
+
+  return NewElement;
+}
+
+struct Element *pop(struct Element *LastElement) {
+  if (LastElement->previousElement != NULL) {
+    return LastElement->previousElement;
+  }
+  return NULL;
+};
 
 int main(void) {
-  printf("Hello World");
+  struct Element *LastElement = calloc(1, sizeof(struct Element));
+  LastElement->previousElement = NULL;
+  LastElement->value = 0;
+
+  printf("Initial Value: %d\n", LastElement->value);
+
+  LastElement = push(LastElement, 1);
+  printf("Value: %d\n", LastElement->value);
+
+  LastElement = push(LastElement, 2);
+  printf("Value: %d\n", LastElement->value);
+
+  LastElement = push(LastElement, 3);
+  printf("Value: %d\n", LastElement->value);
+
+  LastElement = push(LastElement, 4);
+  printf("Value: %d\n", LastElement->value);
+
+  LastElement = pop(LastElement);
+  printf("Value: %d\n", LastElement->value);
+
   return 0;
 }
