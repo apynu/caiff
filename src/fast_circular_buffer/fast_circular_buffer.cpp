dd060944f85781fda0a5846658c72469202534f9
apynu <apynu@duck.com>
2025-04-22 18:53:36 +0200

    not pretty but ok

--- src/fast_circular_buffer/fast_circular_buffer.cpp
+++ src/fast_circular_buffer/fast_circular_buffer.cpp
@@ -2,22 +2,71 @@
 #include <sched.h>
 
 template <class T> class FastCircBuf {
+
   struct Node {
     struct Node *nextnode;
     T value;
   };
 
-  FastCircBuf(const int size) : m_bufsize(size) { m_buf[m_bufsize]; };
+  FastCircBuf(int size) : m_bufsize(size) {
+    m_buf[m_bufsize];
 
-  ~FastCircBuf();
+    // instantiate head node
+    Node *HeadNode = new Node;
+    HeadNode->nextnode = nullptr;
+
+    m_buf[0] = HeadNode;
+  };
+
+  ~FastCircBuf() {
+    for (int i = 0; i < m_bufsize && i < m_occupation; i++) {
+      delete m_buf[i];
+    }
+  };
 
   // pushes an element to a new node until the buffer is full
   // returns false once the buffer is full
   // when the buffer is full a call to 'clear' or 'remove' has
   // to be made to free up more space
-  bool push_back(T) { return false; };
+  bool push_back(T element) {
 
-  void at(int index) {};
+    bool success = true;
+
+    // handle first element/node
+    if (m_occupation == 0) {
+      m_buf[0]->value = element;
+      m_occupation += 1;
+
+    } else {
+      Node *NewNode = new Node;
+      NewNode->value = element;
+      NewNode->nextnode = nullptr;
+
+      // add to array
+      m_buf[m_occupation] = NewNode;
+
+      // previous node points to new node
+      m_buf[m_occupation - 1]->nextnode = NewNode;
+
+      m_occupation += 1;
+
+      // max occupation reached
+      if (m_occupation == m_bufsize - 1) {
+        // point to head node to wrap around
+        NewNode->nextnode = m_buf[0];
+        success = false;
+      }
+    }
+
+    return success;
+  };
+
+  T at(int index) {
+    if (index >= m_bufsize) {
+      return -1;
+    }
+    return m_buf[index];
+  };
 
   // free whole buffer
   void clear() {
@@ -32,8 +81,8 @@ template <class T> class FastCircBuf {
   int size() const { return m_bufsize; };
 
 private:
-  int *m_buf;
-  const int m_bufsize;
+  struct Node *m_buf;
+  int m_bufsize;
   int m_occupation = 0;
 };
 
